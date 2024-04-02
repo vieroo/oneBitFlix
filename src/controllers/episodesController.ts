99d9193602da996a90f0a9b671511df833153a21
Eduardo Viero <duduviero@gmail.com>
2024-04-01 21:53:09 -0300

    Progresso dos episodios

--- src/controllers/episodesController.ts
+++ src/controllers/episodesController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { episodeService } from "../services/episodeService";
+import { AuthenticatedRequest } from "../middlewares/auth";
 
 export const episodesController = {
   // GET /episodes/stram?videoUrl=
-  stream: async(req: Request, res: Response) => {
+  stream: async (req: Request, res: Response) => {
     const { videoUrl } = req.query
 
     try {
@@ -14,6 +15,42 @@ export const episodesController = {
       episodeService.streamEpisodeToResponse(res, videoUrl, range)
 
 
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message})
+      }
+    }
+  },
+
+  // GET /episodes/:id/watchtime
+  getWatchTime: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id
+    const episodeId = req.params.id
+
+    try {
+      const watchTime = await episodeService.getWatchTime(userId, Number(episodeId))
+      return res.json(watchTime)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message})
+      }
+    }
+  },
+
+  // POST /episodes/:id/watchtime
+  setWatchTime: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id
+    const episodeId = Number(req.params.id)
+    const { seconds } = req.body
+
+    try {
+      const watchTime = await episodeService.setWatchTime({
+        episodeId,
+        userId,
+        seconds
+    })
+
+      return res.json(watchTime)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message})
